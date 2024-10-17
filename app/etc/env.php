94d7ccfeb46087660e1abec86a093bebf9c54fd3
12345-Vlad-Bilenko <12345.vlad.b@gmail.com>
2024-10-17 11:44:52 +0300

    comments

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_yy8ifn1'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'noUUatCPiuJG92mSnPjK2Za3jPMDkNab'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f45_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f45_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64b32fg3qoR33H8lL4ybQNrPXLt+Sb+BNib+HeTUzFXNY='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql-8.0',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Wed, 16 Oct 2024 09:24:19 +0000'
+    ]
+];
---
+++ full_deploy_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "maintenance enable"
+php bin/magento maintenance:enable
+
+echo "Removing pub/static/adminhtml..."
+rm -rf pub/static/adminhtml/
+
+echo "Removing pub/static/adminhtml..."
+rm -rf pub/static/adminhtml/
+
+echo "Removing pub/static/frontend..."
+rm -rf pub/static/frontend/
+
+echo "removing deployed_version"
+rm pub/static/deployed_version.txt
+
+echo "Removing var/cache/ var/generation/ var/page_cache/ var/view_preprocessed/"
+rm -rf var/cache/ var/generation/ var/page_cache/ var/view_preprocessed/
+
+echo "Setup:upgrade..."
+php bin/magento setup:upgrade
+
+echo "di:compile..."
+php bin/magento setup:di:compile
+
+echo "static-content:deploy with params..."
+php bin/magento setup:static-content:deploy -f
+
+echo "maintenance disable"
+php bin/magento maintenance:disable
