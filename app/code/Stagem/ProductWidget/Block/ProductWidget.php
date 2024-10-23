3a59874e9479c9f3ce4c2609cae0a550e93d4305
12345-Vlad-Bilenko <12345.vlad.b@gmail.com>
2024-10-23 18:08:58 +0300

    новй код с ГПТ

--- app/code/Stagem/ProductWidget/Block/ProductWidget.php
+++ app/code/Stagem/ProductWidget/Block/ProductWidget.php
@@ -12,34 +12,28 @@ class ProductWidget extends Template implements BlockInterface
     protected $productCollectionFactory;
     protected $categoryRepository;
 
-    
     public function getCategoryId()
     {
-        return (int)$this->getData('category_id');
+        return (int)$this->getData('catid');
     }
-
 
     public function getProductCount()
     {
         return (int)$this->getData('product_count');
     }
 
-    public function __construct
-        (
-            Template\Context $context,
-            CollectionFactory $productCollectionFactory,
-            CategoryRepository $categoryRepository,
-            array $data = []
-        ) 
-    {
+    public function __construct(
+        Template\Context $context,
+        CollectionFactory $productCollectionFactory,
+        CategoryRepository $categoryRepository,
+        array $data = []
+    ) {
         $this->productCollectionFactory = $productCollectionFactory;
         $this->categoryRepository = $categoryRepository;
         parent::__construct($context, $data);
         $this->setTemplate('Stagem_ProductWidget::productwidget.phtml');
-
     }
 
-    
     public function getProductCollection()
     {
         $collection = $this->productCollectionFactory->create();
@@ -48,15 +42,14 @@ class ProductWidget extends Template implements BlockInterface
 
         try {
             $category = $this->categoryRepository->get($categoryId);
-            
             $collection->addAttributeToSelect('*')
-                       ->addCategoryFilter($category) 
-                       ->setPageSize($this->getProductCount()) 
-                       ->setOrder('price', 'DESC'); 
+                       ->addCategoryFilter($category)
+                       ->setPageSize($this->getProductCount())
+                       ->setOrder('price', 'DESC');
         } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-            return [];
+            return null; // Возвращаем null, если категория не найдена
         }
 
         return $collection;
-    }   
+    }
 }
