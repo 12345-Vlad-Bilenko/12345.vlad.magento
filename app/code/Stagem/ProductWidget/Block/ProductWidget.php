635440976313d9112d02a642f22b79f5c1845a25
12345-Vlad-Bilenko <12345.vlad.b@gmail.com>
2024-10-24 17:19:57 +0300

    віджет працює та показує зображення

--- app/code/Stagem/ProductWidget/Block/ProductWidget.php
+++ app/code/Stagem/ProductWidget/Block/ProductWidget.php
@@ -6,20 +6,27 @@ use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Catalog\Model\CategoryRepository;
+use Magento\Catalog\Helper\Image as ImageHelper; // Добавляем хелпер для работы с изображениями
+
 
 class ProductWidget extends Template implements BlockInterface
 {
     protected CollectionFactory $productCollectionFactory;
     protected CategoryRepository $categoryRepository;
+    protected ImageHelper $imageHelper; // Объявляем переменную для хелпера
+
 
     public function __construct(
         Template\Context $context,
         CollectionFactory $productCollectionFactory,
         CategoryRepository $categoryRepository,
+        ImageHelper $imageHelper, // Добавляем хелпер в конструктор
         array $data = []
     ) {
         $this->productCollectionFactory = $productCollectionFactory;
         $this->categoryRepository = $categoryRepository;
+        $this->imageHelper = $imageHelper; // Инициализируем хелпер
+
 
         parent::__construct($context, $data);
         $this->setTemplate('Stagem_ProductWidget::productwidget.phtml');
@@ -56,6 +63,13 @@ class ProductWidget extends Template implements BlockInterface
                        ->addCategoryFilter($category)
                        ->setPageSize($this->getProductCount())
                        ->setOrder('price', 'DESC');
+
+                       // Получаем URL изображения для каждого продукта
+            foreach ($collection as $product) {
+                // Используем хелпер для получения URL изображения
+                $imageUrl = $this->imageHelper->init($product, 'product_base_image')->getUrl();
+                $product->setData('image_url', $imageUrl); // Сохраняем URL изображения в продукт
+            }
 
             return $collection->getItems();
         } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
